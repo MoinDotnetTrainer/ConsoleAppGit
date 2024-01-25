44592b0d7e18faec1d2c125b857cb9be1a118470
moin <onlyonemoin@gmail.com>
2024-01-25 10:09:03 +0530

    alpabranch

--- ConsoleAppGitDemo/Program.cs
+++ ConsoleAppGitDemo/Program.cs
@@ -11,6 +11,7 @@ namespace ConsoleAppGitDemo
         static void Main(string[] args)
         {
             Console.WriteLine("Hello world");
+            Console.WriteLine("alpha");
         }
     }
 }
